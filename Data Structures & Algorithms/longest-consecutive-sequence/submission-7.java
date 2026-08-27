1f9f3f1863841e00ce2b1ea1a8aa6e2225da71e3
Jaeyeon Won <66868132+wjy0524@users.noreply.github.com>
2026-08-28 05:46:27 +0900

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //if nums are empty list, then it should return 0;
+        if(nums.length == 0) return 0;
+        
+        int longest = 1;
+        int maxLongest = 1;
+
+        //일단 nums를 sorting한다 
+        //오름차순으로
+        Arrays.sort(nums);
+ 
+        int idx = 1;
+
+        while(idx < nums.length){
+            if(nums[idx] == nums[idx-1] + 1){
+                longest++;
+                maxLongest = Math.max(maxLongest, longest);
+                idx++;
+            }else if(nums[idx] == nums[idx-1]){
+                idx++;
+            }else{
+                longest = 1;
+                idx++;
+            }
+            
+        }
+
+        return maxLongest;
+
+    
+    }
+}
