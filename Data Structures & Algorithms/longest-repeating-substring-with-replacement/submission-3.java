55c050360046f6d52638adefb58cf58e6d212812
Jaeyeon Won <66868132+wjy0524@users.noreply.github.com>
2026-09-04 18:15:27 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        int[] freq = new int[26];
+
+        for(int right=0; right<s.length(); right++){
+            char c = s.charAt(right);
+            freq[c-'A']++;
+            maxFreq = Math.max(maxFreq, freq[c-'A']);
+            //longest reqpeating character 실패조건
+            if(right-left+1-maxFreq > k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+
+        return maxLen;
+    }
+}
