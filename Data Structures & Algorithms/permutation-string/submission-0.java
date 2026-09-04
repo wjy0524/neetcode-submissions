034edeb24a10309d0c201597872f22498782f2fd
Jaeyeon Won <66868132+wjy0524@users.noreply.github.com>
2026-09-04 18:26:22 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        //pruning
+        if(s1.length() > s2.length()) return false;
+        int[] s1Alphabets = new int[26];
+        int[] s2Window = new int[26];
+        for(char c : s1.toCharArray()){
+            s1Alphabets[c-'a']++;
+        }
+
+        int windowSize = s1.length();
+
+        for(int i=0; i<windowSize; i++){
+            s2Window[s2.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(s1Alphabets, s2Window)){
+            return true;
+        }
+
+        int left = 0;
+        for(int right=windowSize; right<s2.length(); right++){
+            s2Window[s2.charAt(left)-'a']--;
+            s2Window[s2.charAt(right)-'a']++;
+            if(Arrays.equals(s1Alphabets, s2Window)){
+                return true;
+            }
+            left++;
+        }
+
+        return false; 
+    }
+}
